f77acde7b16252aef41c6b7af822566dd1c8c18f
hacel <s@sammar.sa>
2024-09-04 22:11:00 +0300

    saturate colors 1.3

--- lua/tokyonight/colors/day.lua
+++ lua/tokyonight/colors/day.lua
@@ -13,53 +13,56 @@ return function(opts)
   Util.invert(colors)
   colors.bg_dark = Util.blend(colors.bg, 0.9, colors.fg)
 
-  -- vim.print(colors)
-  -- {
-  --   bg = "#e1e2e7",
-  --   bg_dark = "#d0d5e3",
-  --   bg_highlight = "#c4c8da",
-  --   blue = "#2e7de9",
-  --   blue0 = "#7890dd",
-  --   blue1 = "#188092",
-  --   blue2 = "#07879d",
-  --   blue5 = "#006a83",
-  --   blue6 = "#2e5857",
-  --   blue7 = "#92a6d5",
-  --   comment = "#848cb5",
-  --   cyan = "#007197",
-  --   dark3 = "#8990b3",
-  --   dark5 = "#68709a",
-  --   fg = "#3760bf",
-  --   fg_dark = "#6172b0",
-  --   fg_gutter = "#a8aecb",
-  --   git = {
-  --     add = "#4197a4",
-  --     change = "#506d9c",
-  --     delete = "#c47981"
-  --   },
-  --   green = "#587539",
-  --   green1 = "#387068",
-  --   green2 = "#38919f",
-  --   magenta = "#9854f1",
-  --   magenta2 = "#d20065",
-  --   orange = "#b15c00",
-  --   purple = "#7847bd",
-  --   red = "#f52a65",
-  --   red1 = "#c64343",
-  --   teal = "#118c74",
-  --   terminal_black = "#a1a6c5",
-  --   yellow = "#8c6c3e"
-  -- }
+  vim.print(colors)
 
-  -- Overrides
   colors.bg = "#ffffff"
-  colors.fg = "#10103f"
-  colors.orange = "#e15c00"
-  colors.green = "#5aae00"
-  colors.blue1 = "#188092"
-  colors.git.add = "#07c2de"
-  colors.git.change = "#085ce4"
-  colors.git.delete = "#f94457"
+  colors.bg_dark = "#ced5e7"
+  colors.bg_highlight = "#c3c8df"
+  colors.blue = "#0081f8"
+  colors.blue0 = "#6990f4"
+  colors.blue1 = "#00839a"
+  colors.blue2 = "#0089a3"
+  colors.blue5 = "#006c8a"
+  colors.blue6 = "#1d5a59"
+  colors.blue7 = "#8aa6e3"
+  colors.comment = "#808cc1"
+  colors.cyan = "#00739e"
+  colors.dark3 = "#8690bd"
+  colors.dark5 = "#6370a6"
+  colors.fg = "#0060dc"
+  colors.fg_dark = "#5672c2"
+  colors.fg_gutter = "#a5aed4"
+  colors.git.add = "#009aac"
+  colors.git.change = "#416eab"
+  colors.git.delete = "#d2717d"
+  colors.green = "#507727"
+  colors.green1 = "#207269"
+  colors.green2 = "#0094a7"
+  colors.magenta = "#994dff"
+  colors.magenta2 = "#dc0066"
+  colors.orange = "#be5200"
+  colors.purple = "#7a3bda"
+  colors.red = "#ff0062"
+  colors.red1 = "#da2036"
+  colors.teal = "#008f72"
+  colors.terminal_black = "#9fa6ce"
+  colors.yellow = "#926a2d"
 
+  ---@class CyberdreamColorLight
+  ---@field bg "#ffffff"|string
+  ---@field bgAlt "#f7f8f9"|string
+  ---@field bgHighlight "#e9eef2"|string
+  ---@field bg_solid? string
+  ---@field fg "#16181a"|string
+  ---@field grey "#7b8496"|string
+  ---@field blue "#5ea1ff"|string
+  ---@field green "#5eff6c"|string
+  ---@field cyan "#5ef1ff"|string
+  ---@field red "#ff6e5e"|string
+  ---@field yellow "#f1ff5e"|string
+  ---@field magenta "#ff5ef1"|string
+  ---@field pink "#ff5ea0"|string
+  ---@field orange "#ffbd5e"|string
+  ---@field purple "#bd5eff"|string
   return colors
 end
